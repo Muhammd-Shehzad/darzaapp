7e17f692bb7670e4ce45e2f4617e420d97b0141d
Muhammd-Shehzad <127086042+Muhammd-Shehzad@users.noreply.github.com>
2024-07-28 22:33:23 +0500

    half page done

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -8,13 +10,72 @@ class LoginPage extends StatelessWidget {
     return MaterialApp(
       home: SafeArea(
         child: Scaffold(
-          appBar: AppBar(
-            leading: Icon(Icons.account_circle),
-          ),
           body: Container(
-            height: double.infinity,
-            width: double.infinity,
-            decoration: BoxDecoration(color: Colors.white),
+            height: double.infinity.h,
+            width: double.infinity.w,
+            decoration: BoxDecoration(color: Colors.grey),
+            child: Column(
+              children: [
+                Stack(
+                  children: [
+                    Container(
+                      height: 180.h,
+                      width: 500.w,
+                      decoration: BoxDecoration(color: Colors.orange[700]),
+                      child: Image.asset('assets/daraz.png'),
+                    ),
+                    Positioned(
+                      top: 70,
+                      left: 110,
+                      child: Center(
+                        child: Container(
+                          height: 50.h,
+                          width: 180.w,
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(5)),
+                          child: Center(
+                            child: Text(
+                              'Login / Singup ',
+                              style: TextStyle(
+                                  color: Colors.orange[700], fontSize: 20),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Icon(Icons.account_circle_outlined),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text('Hello Welcome Daraz')
+                      ],
+                    )
+                  ],
+                ),
+                Container(
+                  height: 180.h,
+                  width: 500.w,
+                  decoration: BoxDecoration(color: Colors.white),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'My order',
+                        style: TextStyle(color: Colors.black, fontSize: 18),
+                      ),
+                      Text(
+                        'View All>',
+                        style:
+                            TextStyle(color: Colors.orange[700], fontSize: 18),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
